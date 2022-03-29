7ff4dfc931fdc8e245902fa5153a828006cacecc
Simon Bergot <sbergot@cegid.com>
2022-03-29 19:47:23 +0200

    add cyberpunk source folder

---
+++ cyberpunk/run.ps1
@@ -0,0 +1,29 @@
+$Root = $PSScriptRoot
+
+$InformationPreference = "Continue"
+$template = Get-Content "$Root/template.html"
+$outputFolder = "$Root\..\content\cyberpunk"
+$TextInfo = (Get-Culture).TextInfo
+
+function Replace-Content ($file) {
+    $name = $file.BaseName
+    $title = $name -replace '-',' '
+    $title = $TextInfo.ToTitleCase($title)
+    $content = Get-Content $file
+    $content = $content -replace 'style="text-decoration:underline;"',''
+    $output = $template -replace '%body%',$content
+    $output = $output -replace '%title%',$title
+    $output = $output -replace '%name%',$name
+    return $output
+}
+
+if (!(Test-Path $outputFolder)) {
+	New-Item -Path $outputFolder -ItemType "directory"
+}
+
+Get-ChildItem "$Root\raw\*.html" | % {
+    Write-Information "Processing $_"
+    Replace-Content $_ > "$outputFolder\$($_.Name)"
+} 
+
+Copy-Item "$Root\resources\*" $outputFolder
